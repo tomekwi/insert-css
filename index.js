953a33d2772471a1a3f007900c13ca56676c8e96
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-17 13:20:43 +0200

    Accept the option `document`

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var inserted = {};
 module.exports = function (css, options) {
     if (inserted[css]) return;
     inserted[css] = true;
-    
-    var elem = document.createElement('style');
+
+    var doc =
+        (options && options.document) ||
+        (typeof document !== 'undefined' && document)
+    ;
+
+    var elem = doc.createElement('style');
     elem.setAttribute('type', 'text/css');
 
     if ('textContent' in elem) {
@@ -12,8 +17,8 @@ module.exports = function (css, options) {
     } else {
       elem.styleSheet.cssText = css;
     }
-    
-    var head = document.getElementsByTagName('head')[0];
+
+    var head = doc.getElementsByTagName('head')[0];
     if (options && options.prepend) {
         head.insertBefore(elem, head.childNodes[0]);
     } else {
