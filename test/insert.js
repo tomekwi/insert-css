1365eecfb77847f04ce26c91f105c4103ce6e1a2
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-17 13:13:05 +0200

    Pass a jsdom-based `document` in node tests

--- test/insert.js
+++ test/insert.js
@@ -3,6 +3,10 @@ var insertCss = require('../');
 var getStyle = require('computed-style');
 
 var css = 'body { background-color: purple; color: yellow; }';
+var doc =
+    (typeof document !== 'undefined' && document) ||
+    require('jsdom').jsdom('<!DOCTYPE html>').defaultView.document
+;
 
 test(function (t) {
     t.plan(10);
@@ -10,29 +14,29 @@ test(function (t) {
     var before = colors();
     t.ok(before.bg === 'rgba(0,0,0,0)' || before.bg === 'transparent');
     t.ok(before.fg === 'rgb(0,0,0)' || before.fg === '#000000');
-    
-    insertCss(css, { prepend: true });
-    
+
+    insertCss(css, { prepend: true, document: doc });
+
     var after = colors();
     t.ok(after.bg === 'rgb(128,0,128)' || after.bg === 'purple');
     t.ok(after.fg === 'rgb(255,255,0)' || after.fg === 'yellow');
 
     var resetStyle = 'body { background-color: transparent; color: #000000; }';
-    insertCss(resetStyle);
+    insertCss(resetStyle, { document: doc });
 
     var reset = colors();
     t.ok(reset.bg === 'rgba(0,0,0,0)' || reset.bg === 'transparent');
     t.ok(reset.fg === 'rgb(0,0,0)' || reset.fg === '#000000');
 
     var resetStyle = 'body { background-color: green; color: pink; }';
-    insertCss(resetStyle, { prepend: true });
+    insertCss(resetStyle, { prepend: true, document: doc });
 
     var reset = colors();
     t.ok(reset.bg === 'rgba(0,0,0,0)' || reset.bg === 'transparent');
     t.ok(reset.fg === 'rgb(0,0,0)' || reset.fg === '#000000');
 
     var resetStyle = 'body { background-color: yellow; color: purple; }';
-    insertCss(resetStyle, { prepend: false });
+    insertCss(resetStyle, { prepend: false, document: doc });
 
     var reset = colors();
     t.ok(reset.bg === 'rgb(255,255,0)' || reset.bg === 'yellow');
@@ -40,7 +44,7 @@ test(function (t) {
 });
 
 function colors () {
-    var body = document.getElementsByTagName('body')[0];
+    var body = doc.getElementsByTagName('body')[0];
     return {
         bg: getStyle(body, 'backgroundColor').replace(/\s+/g, ''),
         fg: getStyle(body, 'color').replace(/\s+/g, '')
